07a05a07e2b1037beb3d8d57d1c2ae1a02847c2d
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-01-31 15:02:12 -0300

    Upgrade code for solc 0.5

--- src/MakerOtcSupportMethods.sol
+++ src/MakerOtcSupportMethods.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.5.12;
 
 import "ds-math/math.sol";
 
-contract OtcInterface {
+contract OtcLike {
     struct OfferInfo {
         uint              pay_amt;
         address           pay_gem;
@@ -17,57 +17,88 @@ contract OtcInterface {
 }
 
 contract MakerOtcSupportMethods is DSMath {
-    function getOffers(OtcInterface otc, address payToken, address buyToken) public view
-        returns (uint[100] ids, uint[100] payAmts, uint[100] buyAmts, address[100] owners, uint[100] timestamps)
-    {
-        (ids, payAmts, buyAmts, owners, timestamps) = getOffers(otc, otc.getBestOffer(payToken, buyToken));
+    function getOffers(
+        address otc,
+        address payToken,
+        address buyToken
+    ) public view returns (
+        uint[100] memory ids,
+        uint[100] memory payAmts,
+        uint[100] memory buyAmts,
+        address[100] memory owners,
+        uint[100] memory timestamps
+    ) {
+        (ids, payAmts, buyAmts, owners, timestamps) = getOffers(otc, OtcLike(otc).getBestOffer(payToken, buyToken));
     }
 
-    function getOffers(OtcInterface otc, uint offerId) public view
-        returns (uint[100] ids, uint[100] payAmts, uint[100] buyAmts, address[100] owners, uint[100] timestamps)
-    {
+    function getOffers(
+        address otc,
+        uint offerId_
+    ) public view returns (
+        uint[100] memory ids,
+        uint[100] memory payAmts,
+        uint[100] memory buyAmts,
+        address[100] memory owners,
+        uint[100] memory timestamps
+    ) {
+        uint offerId = offerId_;
         uint i = 0;
         do {
-            (payAmts[i],, buyAmts[i],, owners[i], timestamps[i]) = otc.offers(offerId);
-            if(owners[i] == 0) break;
+            (payAmts[i],, buyAmts[i],, owners[i], timestamps[i]) = OtcLike(otc).offers(offerId);
+            if(owners[i] == address(0)) break;
             ids[i] = offerId;
-            offerId = otc.getWorseOffer(offerId);
+            offerId = OtcLike(otc).getWorseOffer(offerId);
         } while (++i < 100);
     }
 
-    function getOffersAmountToSellAll(OtcInterface otc, address payToken, uint payAmt, address buyToken) public view returns (uint ordersToTake, bool takesPartialOrder) {
-        uint offerId = otc.getBestOffer(buyToken, payToken);                        // Get best offer for the token pair
+    function getOffersAmountToSellAll(
+        address otc,
+        address payToken,
+        uint payAmt,
+        address buyToken
+    ) public view returns (
+        uint ordersToTake,
+        bool takesPartialOrder
+    ) {
+        uint offerId = OtcLike(otc).getBestOffer(buyToken, payToken);               // Get best offer for the token pair
         ordersToTake = 0;
         uint payAmt2 = payAmt;
         uint orderBuyAmt = 0;
-        (,,orderBuyAmt,,,) = otc.offers(offerId);
+        (,,orderBuyAmt,,,) = OtcLike(otc).offers(offerId);
         while (payAmt2 > orderBuyAmt) {
             ordersToTake ++;                                                        // New order taken
             payAmt2 = sub(payAmt2, orderBuyAmt);                                    // Decrease amount to pay
             if (payAmt2 > 0) {                                                      // If we still need more offers
-                offerId = otc.getWorseOffer(offerId);                               // We look for the next best offer
-                require(offerId != 0);                                              // Fails if there are not enough offers to complete
-                (,,orderBuyAmt,,,) = otc.offers(offerId);
+                offerId = OtcLike(otc).getWorseOffer(offerId);                      // We look for the next best offer
+                require(offerId != 0, "");                                          // Fails if there are not enough offers to complete
+                (,,orderBuyAmt,,,) = OtcLike(otc).offers(offerId);
             }
-            
         }
         ordersToTake = payAmt2 == orderBuyAmt ? ordersToTake + 1 : ordersToTake;    // If the remaining amount is equal than the latest order, then it will also be taken completely
         takesPartialOrder = payAmt2 < orderBuyAmt;                                  // If the remaining amount is lower than the latest order, then it will take a partial order
     }
 
-    function getOffersAmountToBuyAll(OtcInterface otc, address buyToken, uint buyAmt, address payToken) public view returns (uint ordersToTake, bool takesPartialOrder) {
-        uint offerId = otc.getBestOffer(buyToken, payToken);                        // Get best offer for the token pair
+    function getOffersAmountToBuyAll(
+        address otc,
+        address buyToken,
+        uint buyAmt,
+        address payToken
+    ) public view returns (
+        uint ordersToTake,
+        bool takesPartialOrder
+    ) {
+        uint offerId = OtcLike(otc).getBestOffer(buyToken, payToken);               // Get best offer for the token pair
         ordersToTake = 0;
         uint buyAmt2 = buyAmt;
         uint orderPayAmt = 0;
-        (orderPayAmt,,,,,) = otc.offers(offerId);
+        (orderPayAmt,,,,,) = OtcLike(otc).offers(offerId);
         while (buyAmt2 > orderPayAmt) {
             ordersToTake ++;                                                        // New order taken
             buyAmt2 = sub(buyAmt2, orderPayAmt);                                    // Decrease amount to buy
             if (buyAmt2 > 0) {                                                      // If we still need more offers
-                offerId = otc.getWorseOffer(offerId);                               // We look for the next best offer
-                require(offerId != 0);                                              // Fails if there are not enough offers to complete
-                (orderPayAmt,,,,,) = otc.offers(offerId);
+                offerId = OtcLike(otc).getWorseOffer(offerId);                      // We look for the next best offer
+                require(offerId != 0, "");                                          // Fails if there are not enough offers to complete
+                (orderPayAmt,,,,,) = OtcLike(otc).offers(offerId);
             }
         }
         ordersToTake = buyAmt2 == orderPayAmt ? ordersToTake + 1 : ordersToTake;    // If the remaining amount is equal than the latest order, then it will also be taken completely
--- src/MakerOtcSupportMethods.t.sol
+++ src/MakerOtcSupportMethods.t.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.5.12;
 
 import "ds-test/test.sol";
 import "maker-otc/matching_market.sol";
@@ -8,12 +8,12 @@ import "./MakerOtcSupportMethods.sol";
 contract FakeUser {
     MatchingMarket otc;
 
-    function FakeUser(MatchingMarket otc_) public {
+    constructor(MatchingMarket otc_) public {
         otc = otc_;
     }
 
     function doApprove(address token) public {
-        ERC20(token).approve(otc, uint(-1));
+        ERC20(token).approve(address(otc), uint(-1));
     }
 
     function doOffer(uint amount1, address token1, uint amount2, address token2) public {
@@ -34,23 +34,22 @@ contract MakerOtcSupportMethodsTest is DSTest {
 
         otcSupport = new MakerOtcSupportMethods();
         otc = new MatchingMarket(uint64(now + 1 weeks));
-        otc.addTokenPairWhitelist(weth, mkr);
-        weth.approve(otc);
-        mkr.approve(otc);
+        weth.approve(address(otc));
+        mkr.approve(address(otc));
         user = new FakeUser(otc);
-        user.doApprove(weth);
-        user.doApprove(mkr);
+        user.doApprove(address(weth));
+        user.doApprove(address(mkr));
     }
 
     function createOffers(uint oQuantity, uint mkrAmount, uint wethAmount) public {
         for (uint i = 0; i < oQuantity; i ++) {
-            user.doOffer(wethAmount / oQuantity, weth, mkrAmount / oQuantity, mkr);
+            user.doOffer(wethAmount / oQuantity, address(weth), mkrAmount / oQuantity, address(mkr));
         }
     }
 
     function testProxyGetOffers() public {
         weth.mint(20 ether);
-        weth.transfer(user, 20 ether);
+        weth.transfer(address(user), 20 ether);
         createOffers(1, 3200 ether, 10 ether);
         createOffers(1, 2800 ether, 10 ether);
         uint[100] memory ids;
@@ -58,70 +57,70 @@ contract MakerOtcSupportMethodsTest is DSTest {
         uint[100] memory buyAmts;
         address[100] memory owners;
         uint[100] memory timestamps;
-        (ids, payAmts, buyAmts, owners, timestamps) = otcSupport.getOffers(OtcInterface(otc), weth, mkr);
+        (ids, payAmts, buyAmts, owners, timestamps) = otcSupport.getOffers(address(otc), address(weth), address(mkr));
         assertEq(ids[0], 2);
         assertEq(payAmts[0], 10 ether);
         assertEq(buyAmts[0], 2800 ether);
-        assertEq(owners[0], user);
+        assertEq(owners[0], address(user));
         assertEq(ids[1], 1);
         assertEq(payAmts[1], 10 ether);
         assertEq(buyAmts[1], 3200 ether);
-        assertEq(owners[1], user);
+        assertEq(owners[1], address(user));
         assertEq(owners[2], address(0));
     }
 
     function testProxyGetOffersAmountToSellAllPartialOrder() public {
         weth.mint(20 ether);
-        weth.transfer(user, 20 ether);
+        weth.transfer(address(user), 20 ether);
         createOffers(1, 3200 ether, 10 ether);
         createOffers(1, 2800 ether, 10 ether);
         mkr.mint(4000 ether);
-        mkr.approve(otcSupport, 4000 ether);
+        mkr.approve(address(otcSupport), 4000 ether);
         uint offersToTake;
         bool takesPartialOrder;
-        (offersToTake, takesPartialOrder) = otcSupport.getOffersAmountToSellAll(OtcInterface(otc), mkr, 4000 ether, weth);
+        (offersToTake, takesPartialOrder) = otcSupport.getOffersAmountToSellAll(address(otc), address(mkr), 4000 ether, address(weth));
         assertEq(offersToTake, 1);
         assertTrue(takesPartialOrder);
     }
 
     function testProxyGetOffersAmountToSellAllNoPartialOrder() public {
         weth.mint(20 ether);
-        weth.transfer(user, 20 ether);
+        weth.transfer(address(user), 20 ether);
         createOffers(1, 3200 ether, 10 ether);
         createOffers(1, 800 ether, 4 ether);
         mkr.mint(4000 ether);
-        mkr.approve(otcSupport, 4000 ether);
+        mkr.approve(address(otcSupport), 4000 ether);
         uint offersToTake;
         bool takesPartialOrder;
-        (offersToTake, takesPartialOrder) = otcSupport.getOffersAmountToSellAll(OtcInterface(otc), mkr, 4000 ether, weth);
+        (offersToTake, takesPartialOrder) = otcSupport.getOffersAmountToSellAll(address(otc), address(mkr), 4000 ether, address(weth));
         assertEq(offersToTake, 2);
         assertTrue(!takesPartialOrder);
     }
 
     function testProxyGetOffersAmountToBuyAllPartialOrder() public {
         weth.mint(20 ether);
-        weth.transfer(user, 20 ether);
+        weth.transfer(address(user), 20 ether);
         createOffers(1, 3200 ether, 10 ether);
         createOffers(1, 2800 ether, 10 ether);
         mkr.mint(4400 ether);
-        mkr.approve(otcSupport, 4400 ether);
+        mkr.approve(address(otcSupport), 4400 ether);
         uint offersToTake;
         bool takesPartialOrder;
-        (offersToTake, takesPartialOrder) = otcSupport.getOffersAmountToBuyAll(OtcInterface(otc), weth, 15 ether, mkr);
+        (offersToTake, takesPartialOrder) = otcSupport.getOffersAmountToBuyAll(address(otc), address(weth), 15 ether, address(mkr));
         assertEq(offersToTake, 1);
         assertTrue(takesPartialOrder);
     }
 
     function testProxyGetOffersAmountToBuyAllNoPartialOrder() public {
         weth.mint(15 ether);
-        weth.transfer(user, 15 ether);
+        weth.transfer(address(user), 15 ether);
         createOffers(1, 3200 ether, 10 ether);
         createOffers(1, 2800 ether, 5 ether);
         mkr.mint(4400 ether);
-        mkr.approve(otcSupport, 4400 ether);
+        mkr.approve(address(otcSupport), 4400 ether);
         uint offersToTake;
         bool takesPartialOrder;
-        (offersToTake, takesPartialOrder) = otcSupport.getOffersAmountToBuyAll(OtcInterface(otc), weth, 15 ether, mkr);
+        (offersToTake, takesPartialOrder) = otcSupport.getOffersAmountToBuyAll(address(otc), address(weth), 15 ether, address(mkr));
         assertEq(offersToTake, 2);
         assertTrue(!takesPartialOrder);
     }
